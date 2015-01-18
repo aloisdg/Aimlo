a5c5f31350755e18a083dd7e62f30e2d0c611dd5
aloisdg <aloisdegouvello@live.fr>
2015-01-19 00:30:40 +0100

    Add test

--- Aimlo/Aimlo.cs
+++ Aimlo/Aimlo.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Cache;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
+using AIMLbot;
 
 namespace Aimlo
 {
     public class Aimlo
     {
+        private const string UserId = "CityU.Scm.David";
+        private Bot AimlBot;
+        private User myUser;
+
+        public Aimlo()
+        {
+            AimlBot = new Bot();
+            myUser = new User(UserId, AimlBot);
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            AimlBot.loadSettings();
+            AimlBot.isAcceptingUserInput = false;
+            AimlBot.loadAIMLFromFiles();
+            AimlBot.isAcceptingUserInput = true;
+        }
+
+        public string getOutput(string input)
+        {
+            Request request = new Request(input, myUser, AimlBot);
+            Result result = AimlBot.Chat(request);
+            return result.Output;
+        }
     }
 }
---
+++ AimloTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AimloTest
+{
+    class Program
+    {
+        static Aimlo.Aimlo _aimlo;
+        static void Main(string[] args)
+        {
+            _aimlo = new Aimlo.Aimlo();
+            TalkWithAimlo();
+        }
+
+
+        static void TalkWithAimlo()
+        {
+            var input = Console.ReadLine();
+            if (input == "exit")
+                return;
+            var output = _aimlo.getOutput(input);
+            Console.WriteLine(output);
+            TalkWithAimlo();
+        }
+    }
+}
